4c7ecef08320befef84ff61b3561954b9b9fb4a6
u31510 <u31510@P31510>
2022-10-20 16:23:13 +1000

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Rassokhin_PR4_YP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void z_odin(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.Show();
+        }
+
+        private void z_dva(object sender, RoutedEventArgs e)
+        {
+            Window2 window2 = new Window2();
+            window2.Show();
+        }
+
+        private void z_Tri(object sender, RoutedEventArgs e)
+        {
+            Window3 window3 = new Window3();
+            window3.Show();
+        }
+
+        private void z_Hetire(object sender, RoutedEventArgs e)
+        {
+            Window4 window4 = new Window4();
+            window4.Show();
+        }
+
+        private void z_Payti(object sender, RoutedEventArgs e)
+        {
+            Window5 window5 = new Window5();
+            window5.Show();
+        }
+
+        private void z_Shesti(object sender, RoutedEventArgs e)
+        {
+            Window6 window6 = new Window6();
+            window6.Show();
+        }
+
+        private void z_Seni(object sender, RoutedEventArgs e)
+        {
+            Window7 window7 = new Window7();
+            window7.Show();
+        }
+
+        private void z_Voseni(object sender, RoutedEventArgs e)
+        {
+            Window8 window8 = new Window8();
+            window8.Show();
+        }
+
+        private void z_Devyati(object sender, RoutedEventArgs e)
+        {
+            Window9 window9 = new Window9();
+            window9.Show();
+        }
+
+        private void z_Desayti(object sender, RoutedEventArgs e)
+        {
+            Window10 window10 = new Window10();
+            window10.Show();
+        }
+
+        private void z_Odinadhati(object sender, RoutedEventArgs e)
+        {
+            Window11 window11 = new Window11();
+            window11.Show();
+        }
+
+        private void z_Dvinadhati(object sender, RoutedEventArgs e)
+        {
+            Window12 window12 = new Window12();
+            window12.Show();
+        }
+
+        private void z_Trinabhati(object sender, RoutedEventArgs e)
+        {
+            Window13 window13 = new Window13();
+            window13.Show();
+        }
+
+        private void z_Hetirnadhati(object sender, RoutedEventArgs e)
+        {
+            Window14 window14 = new Window14();
+            window14.Show();
+        }
+
+        private void z_Pitnadhati(object sender, RoutedEventArgs e)
+        {
+            Window15 window15 = new Window15();
+            window15.Show();
+        }
+
+        private void z_Hesnadhati(object sender, RoutedEventArgs e)
+        {
+            Window16 window16 = new Window16();
+            window16.Show();
+        }
+
+        private void z_Seninadhati(object sender, RoutedEventArgs e)
+        {
+            Window17 window17 = new Window17();
+            window17.Show();
+        }
+
+        private void z_Voseminadhati(object sender, RoutedEventArgs e)
+        {
+            Window18 window18 = new Window18();
+            window18.Show();
+        }
+
+        private void z_Devetinadhati(object sender, RoutedEventArgs e)
+        {
+            Window19 window19 = new Window19();
+            window19.Show();
+        }
+
+        private void z_Dvadhati(object sender, RoutedEventArgs e)
+        {
+            Window20 window20 = new Window20();
+            window20.Show();
+        }
+
+        private void z_DvadhatiOdin(object sender, RoutedEventArgs e)
+        {
+            Window21 window21 = new Window21();
+            window21.Show();
+        }
+
+        private void z_DvadhatiTri(object sender, RoutedEventArgs e)
+        {
+            Window22 window22 = new Window22();
+            window22.Show();
+        }
+
+        private void z_DvadhatiDva(object sender, RoutedEventArgs e)
+        {
+            Window23 window23 = new Window23();
+            window23.Show();
+        }
+    }
+}
